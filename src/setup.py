3d6068bc4d422872f785b78beeef56aec2ac0567
Jakob S Kottmann <jakob.kottmann@gmail.com>
2020-09-21 15:24:26 -0400

    update workflow

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,10 @@ import setuptools
 setuptools.setup(
     name                            = "timtaylor",
     packages                        = setuptools.find_packages(where = "python"),
-    install_requires                = [ 'tequila @ git+https://github.com/aspuru-guzik-group/tequila.git@master-setup-fix#egg=tequila'],
+    install_requires                = [ 'tequila @ git+https://github.com/aspuru-guzik-group/tequila.git@master-setup-fix#egg=tequila',
+        'z-quantum-core',
+        'qe-openfermion', # needed for qe-psi4
+        'qe-psi4'],
     package_dir                     = {
         "" : "python"
     },
